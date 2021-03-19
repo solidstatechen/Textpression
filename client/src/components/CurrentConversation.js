bba5c98bd23df592402718c915478c7b8d927f32
Oli <oli@Olis-Macbook-Pro.local>
2021-03-19 18:30:00 +0000

    scollbar added to current conversation. scroll bare resets to most recent message when enter pressed

--- client/src/components/CurrentConversation.js
+++ client/src/components/CurrentConversation.js
@@ -1,11 +1,16 @@
 
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect, useCallback } from 'react'
 import { Form, InputGroup, Button} from 'react-bootstrap'
 import { useConversations } from '../Contexts/ConversationsProvider'
 
 export default function CurrentConversation() {
     const [text, setText] = useState('')
     const { sendMessage, selectedConversation } = useConversations()
+    const setRef = useCallback(node => {
+        if (node){
+            node.scrollIntoView({ smooth:true })
+            }
+        }, [])
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -15,14 +20,18 @@ export default function CurrentConversation() {
         setText('')
     }
 
+   
+
     return (
         <div className="d-flex flex-column flex-grow-1">
             <div className="flex-grow-1 overflow-auto">
-                <div className="h-100 d-flex flex-column align-items-start justify-content-end px-3">
+                <div className="d-flex flex-column align-items-start justify-content-end px-3">
                     {selectedConversation.messages.map((message, index) => {
+                        const lastMessage = selectedConversation.messages.length -1 === index
                         return (
-                            <div key={index} className="my-1 d-flex flex-column">
-                                
+                            
+                            <div key={index} ref={lastMessage ? setRef : null} className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end' : ''}`}>
+
                                 <div className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>
                                     {message.text}
                                 </div>
