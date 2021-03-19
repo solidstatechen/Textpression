81b32902c442e2d06dbd712acc8a16f43acf5836
Oli <oli@Olis-Macbook-Pro.local>
2021-03-19 16:59:54 +0000

    Conversations displaying by contact in conversation bar. Contacts displaying in Contact tab. conversations ordered by index

---
+++ client/src/Contexts/ConversationsProvider.js
@@ -0,0 +1,52 @@
+import React, { useContext, useState } from 'react'
+import context from 'react-bootstrap/esm/AccordionContext'
+import useLocalStorage from '../hooks/useLocalStorage'
+import { useContacts } from './ContactsProvider'
+
+
+const ConversationsContext = React.createContext()
+
+export function useConversations() {
+    return useContext(ConversationsContext)
+}
+
+
+export function ConversationsProvider({ children }) {
+
+    const [conversations, setConversations] = useLocalStorage('conversations', [])
+    const { contacts } = useContacts()
+    const [selectedConversationIndex, setSelectedConversationIndex] = useState(0)
+
+    function createConversation(recipients){
+        setConversations(prevConversations => {
+
+            return [...prevConversations, {recipients, messages: [] }]
+        })
+    }
+
+    const formattedConversations = conversations.map((conversation, index) => {
+        const recipients = conversation.recipients.map(recipient => {
+            const contact = contacts.find(contact => {
+                return contact.id === recipient
+            })
+            const name = (contact && contact.name) || recipient
+            return { id: recipient, name }
+        })
+        const selected = index === selectedConversationIndex
+        return {...conversation, recipients, selected }
+    })
+
+    const value = {
+        conversations:formattedConversations,
+        selectConversationIndex: setSelectedConversationIndex,
+        createConversation
+
+    }
+
+
+    return (
+        <ConversationsContext.Provider value={value}>
+            {children}
+        </ConversationsContext.Provider>
+    )
+}
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 import useLocalStorage from '../hooks/useLocalStorage';
 import './App.css';
 import Login from './login.js';
+import Dashboard from './Dashboard'
+import { ContactsProvider } from '../Contexts/ContactsProvider';
+import { ConversationsProvider } from '../Contexts/ConversationsProvider';
 
 function App() {
   const [id, setId] = useLocalStorage('id')
 
+  const dashboard = (
+    <ContactsProvider>
+      <ConversationsProvider>
+        <Dashboard id={id}></Dashboard>
+      </ConversationsProvider>
+    </ContactsProvider>
+  )
+
   return (
-    <>
-       {id}
-       <Login onIdSubmit={setId}/>
-    </>  
+    
+       id ? dashboard :  <Login onIdSubmit={setId}/> 
+      
+      
   )
 }
 
