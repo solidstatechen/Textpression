623c881cf2f4017c97b9a763bf281120ee2d7936
Oli <oli@Olis-Macbook-Pro.local>
2021-03-19 18:14:55 +0000

    conversationProvider extended. Conversation now displys on dashboard. user can add text to conversation

--- client/src/Contexts/ConversationsProvider.js
+++ client/src/Contexts/ConversationsProvider.js
@@ -11,7 +11,7 @@ export function useConversations() {
 }
 
 
-export function ConversationsProvider({ children }) {
+export function ConversationsProvider({ id, children }) {
 
     const [conversations, setConversations] = useLocalStorage('conversations', [])
     const { contacts } = useContacts()
@@ -24,6 +24,38 @@ export function ConversationsProvider({ children }) {
         })
     }
 
+    function addMessageToConversation({recipients, text, sender}){
+        setConversations(prevConversations => {
+            let madeChange = false
+            const newMessage = {sender, text }
+            const newConversations = prevConversations.map
+            (conversation => {
+                if (arrayEquality(conversation.recipients, recipients))
+                {
+                    madeChange = true
+                    return {
+                        ...conversation, 
+                        messages: [...conversation.messages, newMessage]
+                    }
+                }
+                return conversation 
+            })
+
+            if (madeChange){
+                return newConversations
+            } else {
+                return [
+                    ...prevConversations,
+                    { recipients, messages: [newMessage]}
+                ]
+            }
+        })
+    }
+
+    function sendMessage(recipients, text){
+        addMessageToConversation({ recipients, text, sender: id })
+    }
+
     const formattedConversations = conversations.map((conversation, index) => {
         const recipients = conversation.recipients.map(recipient => {
             const contact = contacts.find(contact => {
@@ -32,12 +64,22 @@ export function ConversationsProvider({ children }) {
             const name = (contact && contact.name) || recipient
             return { id: recipient, name }
         })
+        const messages = conversation.messages.map(message => {
+            const contact = contacts.find(contact => {
+                return contact.id === message.sender
+            })
+            const name = (contact && contact.name) || message.sender
+            const fromMe = id === message.sender
+            return { ...message, senderName: name, fromMe }
+        })
         const selected = index === selectedConversationIndex
-        return {...conversation, recipients, selected }
+        return {...conversation, messages, recipients, selected }
     })
 
     const value = {
         conversations:formattedConversations,
+        selectedConversation: formattedConversations [selectedConversationIndex],
+        sendMessage,
         selectConversationIndex: setSelectedConversationIndex,
         createConversation
 
@@ -49,4 +91,16 @@ export function ConversationsProvider({ children }) {
             {children}
         </ConversationsContext.Provider>
     )
+}
+
+function arrayEquality(a, b){
+    if (a.length !== b.length) return false
+
+    a.sort()
+    b.sort()
+
+    return a.every((element, index) => {
+        return element === b[index]
+    })
+
 }
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useConversations } from '../Contexts/ConversationsProvider.js'
+import CurrentConversation from './CurrentConversation.js'
 import Sidebar from './Sidebar.js'
  
 
 export default function Dashboard({ id }) {
+
+    const { selectedConversation } = useConversations()
+
+
+
     return (
         <div className="d-flex" style={{ height: '100vh'}}>
             <Sidebar id ={id}/>
+            {selectedConversation && <CurrentConversation/>}
         </div>
     )
 }
